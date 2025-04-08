0c0e49c127076e40cf63c8ed742dc09ead93736e
Zeev <joseph.zeev@zim.com>
2025-04-08 16:04:12 +0200

    chore(deps): remove dotenv-kotlin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,14 +23,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
-
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")  // For Mockito Kotlin support
+	testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 	testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
@@ -46,14 +44,6 @@ tasks.test {
 		showStandardStreams = true
 	}
 }
-
-
-
-//allOpen {
-//	annotation("jakarta.persistence.Entity")
-//	annotation("jakarta.persistence.MappedSuperclass")
-//	annotation("jakarta.persistence.Embeddable")
-//}
 
 tasks.withType<Test> {
 	useJUnitPlatform()
